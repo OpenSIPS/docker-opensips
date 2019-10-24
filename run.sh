a8dd55f31e1a7d1d99716863d6ae4c41ecaa034d
Razvan Crainea <razvan@opensips.org>
2019-10-24 09:54:20 +0300

    Run opensips in foreground
    Close #1

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@
 HOST_IP=$(ip route get 8.8.8.8 | head -n +1 | tr -s " " | cut -d " " -f 7)
 
 sed -i "s/listen=.*/listen=udp:${HOST_IP}:5060/g" /etc/opensips/opensips.cfg
-sed -i "s/log_stderror=no/log_stderror=yes/g" /etc/opensips/opensips.cfg
 
-/etc/init.d/opensips start
-
-journalctl -f -u opensips
+# skip syslog and run opensips at stderr
+/usr/sbin/opensips -FE
