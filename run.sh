ad8cd3515c502b03a327aa5f2f7cf5bc2c2a2250
Bob Wang <bobw@exetel.com.au>
2020-07-01 11:48:47 +1000

    add modules options

--- run.sh
+++ run.sh
@@ -2,7 +2,12 @@
 
 HOST_IP=$(ip route get 8.8.8.8 | head -n +1 | tr -s " " | cut -d " " -f 7)
 
-sed -i "s/listen=.*/listen=udp:${HOST_IP}:5060/g" /etc/opensips/opensips.cfg
+sed -i "s/^listen.*5060/listen = udp:${HOST_IP}:5060/g" /etc/opensips/opensips.cfg
+
+OPENSIPS_ADVERTISED_ADDRESS=$OPENSIPS_ADVERTISED_ADDRESS
+if [ ! -z ${OPENSIPS_ADVERTISED_ADDRESS} ] && [ `grep -c advertised_address /etc/opensips/opensips.cfg` -eq 0 ] ; then
+    sed -i "/listen = udp/a\advertised_address=${OPENSIPS_ADVERTISED_ADDRESS}" /etc/opensips/opensips.cfg
+fi
 
 # skip syslog and run opensips at stderr
 /usr/sbin/opensips -FE
