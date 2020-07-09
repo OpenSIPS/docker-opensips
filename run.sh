37fa85418f7bd6d896a939942f44484d75864215
Bob Wang <bobw@exetel.com.au>
2020-07-09 11:07:44 +1000

    now script only updates the listen=udp part when starts

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ sed -i "s/RUN_OPENSIPS=no/RUN_OPENSIPS=yes/g" /etc/default/opensips
 sed -i "s/DAEMON=\/sbin\/opensips/DAEMON=\/usr\/sbin\/opensips/g" /etc/init.d/opensips
 
 HOST_IP=$(ip route get 8.8.8.8 | head -n +1 | tr -s " " | cut -d " " -f 7)
-sed -i "s/listen=.*/listen=udp:${HOST_IP}:5060/g" /etc/opensips/opensips.cfg
+sed -i "s/^listen=udp.*5060/listen=udp:${HOST_IP}:5060/g" /etc/opensips/opensips.cfg
 
 # skip syslog and run opensips at stderr
 /usr/sbin/opensips -FE
