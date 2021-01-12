74532f0731b9793b8337105a433abe73469087b8
Razvan Crainea <razvan@opensips.org>
2021-01-12 19:15:29 +0200

    Match either socket or listen
    This way we can support both versions of listeners, for older versions
    as well.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ sed -i "s/RUN_OPENSIPS=no/RUN_OPENSIPS=yes/g" /etc/default/opensips
 sed -i "s/DAEMON=\/sbin\/opensips/DAEMON=\/usr\/sbin\/opensips/g" /etc/init.d/opensips
 
 HOST_IP=$(ip route get 8.8.8.8 | head -n +1 | tr -s " " | cut -d " " -f 7)
-sed -i "s/^socket=udp.*5060/socket=udp:${HOST_IP}:5060/g" /etc/opensips/opensips.cfg
+sed -i "s/^\(socket\|listen\)=udp.*5060/\1=udp:${HOST_IP}:5060/g" /etc/opensips/opensips.cfg
 
 # skip syslog and run opensips at stderr
 /usr/sbin/opensips -FE
